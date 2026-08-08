f1e7acf0585531020e13a795289176bccb45d42b
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-08 14:20:20 -0400

    Adicionando arquivos

---
+++ GIT/Arquivos JS/Apredendo Git e Github.js
@@ -0,0 +1,30 @@
+//Aprendendo sobre Git e GitHub e treinando com eles
+//Criando um repositório
+//Comando para criar um repositório: GIT INIT
+//GIT STATUS - Mostra o que está sendo controlado pelo git
+//GIT ADD - Adiciona arquivos ao controle de versão
+//Exemplo: git add nome_do_arquivo
+//OU enviar todos os arquivos para o controle de versão:
+//GIT ADD .
+//Comando para criar um repositório: GIT INIT
+//GIT STATUS - Mostra o que está sendo controlado pelo git
+//GIT ADD - Adiciona arquivos ao controle de versão
+//Exemplo: git add nome_do_arquivo
+//OU enviar todos os arquivos para o controle de versão:
+//GIT ADD .
+//Commits são uma forma de salvar as alterações feitas no repositório.
+//Exemplo:
+//git commit -m "Adicionando arquivo"
+//-m é para inserir uma mensagem.
+//PARA COMMIT em um arquivo específico:
+//git add nome_do_arquivo
+//git commit -m "Adicionando arquivo"
+//Para commit em todos os arquivos:
+//git add .
+//git commit -m "Adicionando arquivo"
+
+var nome = "João";
+var idade = 20;
+var email = "joao@email.com";
+
+console.log("Olá " + nome + ", você tem " + idade + " anos e seu e-mail é " + email);
