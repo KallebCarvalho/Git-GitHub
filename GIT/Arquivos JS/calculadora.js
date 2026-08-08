986abe169ab451e31377dd6ed47a8a279aa45071
KallebCarvalho <kallebcarvalhocontato@gmail.com>
2026-08-08 14:56:16 -0400

    Apredendo os comandos basicos

---
+++ GIT/Arquivos JS/calculadora.js
@@ -0,0 +1,15 @@
+//Neste desafio vamos aprender a criar uma calculadora
+//Vamos criar um arquivo JS chamado calculadora.js
+//Vamos criar uma função chamada calculadora.
+
+var soma = function(a, b) {
+  return a + b;
+};
+
+console.log(soma(5, 5));    //10
+
+//Vamos criar outra função chamada subtração.
+ var subtração = function (a, b) {
+   return a - b;
+ };
+ console.log(subtração(5, 3));    //2
